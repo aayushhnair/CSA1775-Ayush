b996319d36074eb48c5f9f45347c6fbcee5e23ea
Ayush V Nair <96865576+xakvn@users.noreply.github.com>
2022-09-27 14:23:48 +0530

    Update Vaccum Cleaner.py

--- Vaccum Cleaner.py
+++ Vaccum Cleaner.py
@@ -1,33 +1,33 @@
-import random 
-def display(room): 
-	print(room) 
-room = [ [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], 
-print("All the r000m are dirty") 
-display(room) 
-x =0 
-y= 0 
-while x < 4:
-	 while y < 4:
-	 	room[x][y] = random.choice([0,1])
- 	 	y+=1
- x+=1
- y=0 
-print("Before cleaning the room I detect all of these random dirts")
-display(room) 
-x =0 
-y= 0 
-z=0 
-while x < 4:
- 	while y < 4:
- 		if room[x][y] = 1:
-			 print("Vaccum in this location now,",x, y)
- 			room[x][y) = 0
- 			print("cleaned", x, y)
-			 z+=1 
-		y+=1 
-	x+=1
-	y=0 
-pro= (100-((z/16)*100))
-print("Room is clean now, Thanks for using : 3710933") 
-display(room) 
-print('performance=',pro,'%') 
+import random
+def display(room):
+    print(room)
+room=[[1 for a in range(4)]for b in range(4)]
+print("ALL THE ROOMS ARE DIRTY")
+display(room)
+x=0
+y=0
+while x<4:
+    while y<4:
+        room[x][y]=random.choice([0,1])
+        y+=1  
+    x+=1
+    y=0
+print("BEFORE CLEANING THE ROOM 1 DETECT ALL OF THESE RANDOM DIRTS")
+display(room)
+x=0
+y=0
+z=0
+while x<4:
+    while y<4:
+        if room[x][y]==1:
+            print("VACUUM IN THIS LOCATION NOW, ",x,y)
+            room[x][y]=0
+            print("CLEANED ",x,y)
+            z+=1
+        y+=1
+    x+=1
+    y=0
+pro=(100-((z/16)*100))
+print("ROOM IS CLEAN NOW")
+display(room)
+print("PERFORMANCE --> ", pro,"%")
